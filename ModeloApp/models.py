d1c6fd35f918b61671d78150459797ac7fc28cf0
Luciano Berrios <lberrios.1402@gmail.com>
2024-09-24 17:18:46 -0300

    Fin de la clase
    No funciona aun

--- ModeloApp/models.py
+++ ModeloApp/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+class Employee(models.Model):
+    nombre1 = models.CharField(max_length=50)
+    email1 = models.CharField(max_length=50)
+    fono = models.CharField(max_length=25)
+
