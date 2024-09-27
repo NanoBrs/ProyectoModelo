7d33e16bbe1a708c4f49ffd8dba1101203b47288
Luciano Berrios <lberrios.1402@gmail.com>
2024-09-27 12:24:20 -0300

    Clase 27-09-2024
    Se concreta la primera template,falta agregar diseño con boostrap

--- ProyectoM/ProyectoApp/views.py
+++ ProyectoM/ProyectoApp/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
-
+from ProyectoApp.models import Employee
 # Create your views here.
+def employeeData(request):
+    empleados = Employee.objects.all()
+    data = {'empleados': empleados}
+    return render(request,'ProyectoApp/empleados.html',data)
+
--- ProyectoM/ProyectoM/urls.py
+++ ProyectoM/ProyectoM/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from ProyectoApp import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('empleados/',views.employeeData)
 ]
