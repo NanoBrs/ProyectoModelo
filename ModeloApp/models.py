088766dcb4719035996be0473a1077bf6122e1f0
Luciano Berrios <lberrios.1402@gmail.com>
2024-09-27 11:27:12 -0300

    Arregladoski

--- ModeloApp/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-# Create your models here.
-
-class Employee(models.Model):
-    nombre1 = models.CharField(max_length=50)
-    email1 = models.CharField(max_length=50)
-    fono = models.CharField(max_length=25)
-
--- ProyectoM/ProyectoApp/apps.py
+++ ProyectoM/ProyectoApp/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class ModeloappConfig(AppConfig):
+class ProyectoappConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'ModeloApp'
+    name = 'ProyectoApp'
